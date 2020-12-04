d2e0053834203428d710a51c69d0e7f5ff13cb25
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2020-12-03 23:20:45 -0500

    Update col_deconv_main.py

--- Functions/col_deconv_main.py
+++ Functions/col_deconv_main.py
@@ -14,38 +14,33 @@ from skimage.morphology import disk,dilation ,erosion
 
 '''Stain deconvolution'''
 ''' ====================='''
-rgb_from_hrd = np.array([[0.644, 0.710, 0.285],
+color_vector = np.array([[0.644, 0.710, 0.285],
                          [0.0326, 0.873, 0.487],
                          [0.270, 0.562, 0.781]])
 
-hrd_from_rgb = linalg.inv(rgb_from_hrd)
+converted = linalg.inv(color_vector)
 
-def separate_stains(rgb, color_deconv_vector):
-    rgb = dtype.img_as_float(rgb, force_copy=True)
-    rgb += 2
-    stains = np.dot(np.reshape(-np.log(rgb), (-1, 3)), color_deconv_vector)
-    return np.reshape(stains, rgb.shape)
-
-def stainspace_to_2d_array(ihc_xyz, channel):
-    rescale = rescale_intensity(ihc_xyz[:, :, channel], out_range=(0,1))
-    stain_array = np.dstack((np.zeros_like(rescale), rescale, rescale))
-    grey_array = rgb2grey(stain_array)
-    return grey_array
-
-def col_deconv(ihc_rgb,bias,ki67bias,DiscSize):
-    ihc_hrd = separate_stains(ihc_rgb, hrd_from_rgb)
+def col_deconv(Img,bias,ki67bias,DiscSize):
+    Img = dtype.img_as_float(Img, force_copy=True)
+    Img += 2
+    stains = np.dot(np.reshape(-np.log(Img), (-1, 3)), converted)
+    ihc_hrd = np.reshape(stains, Img.shape)
     print("Color deconvolution...")
     
-    permred_Gray_Array = stainspace_to_2d_array(ihc_hrd, 1)
-    DAB_Grey_Array = stainspace_to_2d_array(ihc_hrd, 2)
+    synaptophysin_1 = rescale_intensity(ihc_hrd[:, :, 1], out_range=(0,1))
+    synaptophysin_final = rgb2grey(np.dstack((np.zeros_like(synaptophysin_1), synaptophysin_1, synaptophysin_1)))
+    
+    DAB_1 = rescale_intensity(ihc_hrd[:, :, 2], out_range=(0,1))
+    DAB_final = rgb2grey(np.dstack((np.zeros_like(DAB_1), DAB_1, DAB_1)))
+    
     
     print("Thresholding ki67...")
     
-    _,thre_ki67 = cv2.threshold(DAB_Grey_Array,ki67bias,255,cv2.THRESH_BINARY)
+    _,thre_ki67 = cv2.threshold(DAB_final,ki67bias,255,cv2.THRESH_BINARY)
     
     print("Otsu thresholding of synaptophysin...")
     
-    ret,blur_red =  cv2.threshold((permred_Gray_Array),bias,255,cv2.THRESH_BINARY)
+    ret,blur_red =  cv2.threshold((synaptophysin_final),bias,255,cv2.THRESH_BINARY)
     blur_red = dilation(blur_red,disk(DiscSize)) 
     blur_red_mr = erosion(blur_red,disk(DiscSize)) 
     ki_mask_mr2 = cv2.bitwise_and((thre_ki67), (blur_red_mr))
